2717fdaab8c0310004f734d93ac0a3beded63bc3
Moycat <i@moy.cat>
2024-02-08 22:29:13 -0800

    fix sql file

--- sql/20240208_init.sql
+++ sql/20240208_init.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS clients
     client_id       TEXT PRIMARY KEY NOT NULL,
     client_secret   TEXT NOT NULL,
     redirect_uri    TEXT NOT NULL
-)
+);
 
 CREATE TABLE IF NOT EXISTS users
 (
@@ -12,4 +12,4 @@ CREATE TABLE IF NOT EXISTS users
     email       TEXT,
     name        TEXT,
     picture     TEXT
-)
+);
